a1e087b962a4eb90eccfe2097e4ebf557ff870f2
Mohamed Amine <mohamedaminebenhmida4@gmail.com>
2022-10-18 10:54:00 +0100

    final-state

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express=require('express')
 const app=express()
 const port=5000;
-const date = new Date();
 
-  if ((date.getDay()>0)&&(date.getDay()<6)&&(date.getHours()>=9)&&(date.getHours()<=17))
-  {
+const logger=(req,res,next)=>{
+const date = new Date();
+ if ((date.getDay()===0)||(date.getDay()===6) || (date.getHours()<9)||(date.getHours()>=17)){
+return res.sendFile(__dirname+'/Vacation.html')
+ }
+ next()
+}
+
+app.use(logger)
+ 
+  
 app.get('/', (req, res) => {
   res.sendFile(__dirname+'/index.html')
 })
@@ -18,22 +26,22 @@ app.get('/Services.css', (req, res) => {
   res.sendFile(__dirname+'/Services.css')
 })
 
-  }
-  else {
-    app.get('/', (req, res) => {
-res.sendFile(__dirname+'/Vacation.html')
-  })
-  app.get('/Contact', (req, res) => {
-  res.sendFile(__dirname+'/Vacation.html')
-})
-app.get('/Services', (req, res) => {
-  res.sendFile(__dirname+'/Vacation.html')
-})
-app.get('/Vacation.css', (req, res) => {
-  res.sendFile(__dirname+'/Vacation.css')
-})
+  
+//   else {
+//     app.get('/', (req, res) => {
 
-}
+//   })
+//   app.get('/Contact', (req, res) => {
+//   res.sendFile(__dirname+'/Vacation.html')
+// })
+// app.get('/Services', (req, res) => {
+//   res.sendFile(__dirname+'/Vacation.html')
+// })
+// app.get('/Vacation.css', (req, res) => {
+//   res.sendFile(__dirname+'/Vacation.css')
+// })
+
+// }
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
